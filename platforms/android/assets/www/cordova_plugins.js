449faefde8c80861c823f40d403d94bedc3660ee
hazems <hazems@apache.org>
2014-06-02 22:19:13 +0300

    Adding Storage Demo

--- platforms/android/assets/www/cordova_plugins.js
+++ platforms/android/assets/www/cordova_plugins.js
@@ -184,6 +184,73 @@ module.exports = [
         ]
     },
     {
+        "file": "plugins/org.apache.cordova.contacts/www/contacts.js",
+        "id": "org.apache.cordova.contacts.contacts",
+        "clobbers": [
+            "navigator.contacts"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/Contact.js",
+        "id": "org.apache.cordova.contacts.Contact",
+        "clobbers": [
+            "Contact"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactAddress.js",
+        "id": "org.apache.cordova.contacts.ContactAddress",
+        "clobbers": [
+            "ContactAddress"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactError.js",
+        "id": "org.apache.cordova.contacts.ContactError",
+        "clobbers": [
+            "ContactError"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactField.js",
+        "id": "org.apache.cordova.contacts.ContactField",
+        "clobbers": [
+            "ContactField"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactFindOptions.js",
+        "id": "org.apache.cordova.contacts.ContactFindOptions",
+        "clobbers": [
+            "ContactFindOptions"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactName.js",
+        "id": "org.apache.cordova.contacts.ContactName",
+        "clobbers": [
+            "ContactName"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactOrganization.js",
+        "id": "org.apache.cordova.contacts.ContactOrganization",
+        "clobbers": [
+            "ContactOrganization"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.contacts/www/ContactFieldType.js",
+        "id": "org.apache.cordova.contacts.ContactFieldType"
+    },
+    {
+        "file": "plugins/org.apache.cordova.battery-status/www/battery.js",
+        "id": "org.apache.cordova.battery-status.battery",
+        "clobbers": [
+            "navigator.battery"
+        ]
+    },
+    {
         "file": "plugins/org.apache.cordova.file/www/DirectoryEntry.js",
         "id": "org.apache.cordova.file.DirectoryEntry",
         "clobbers": [
@@ -327,6 +394,8 @@ module.exports.metadata =
     "org.apache.cordova.globalization": "0.2.8-dev",
     "org.apache.cordova.vibration": "0.3.9-dev",
     "org.apache.cordova.dialogs": "0.2.8-dev",
+    "org.apache.cordova.contacts": "0.2.11-dev",
+    "org.apache.cordova.battery-status": "0.2.9-dev",
     "org.apache.cordova.file": "1.1.0"
 }
 // BOTTOM OF METADATA
--- www/js/api/NotificationManager.js
+++ www/js/api/NotificationManager.js
@@ -8,16 +8,16 @@ var NotificationManager = (function () {
               navigator.notification.alert(message, callback, title, buttonName);
           },
           showConfirm: function (message, callback, title, buttonLabels) {
-        	  navigator.notification.confirm(message, callback, title, buttonLabels);
+              navigator.notification.confirm(message, callback, title, buttonLabels);
           },
           showPrompt: function (message, callback, title, buttonLabels, defaultText) {
-        	  navigator.notification.prompt(message, callback, title, buttonLabels, defaultText);
+              navigator.notification.prompt(message, callback, title, buttonLabels, defaultText);
           },
           beep: function (times) {
-        	  navigator.notification.beep(times);
+              navigator.notification.beep(times);
           },
           vibrate: function (milliseconds) {
-        	  navigator.notification.vibrate(milliseconds);
+              navigator.notification.vibrate(milliseconds);
           }          
       };
   };
