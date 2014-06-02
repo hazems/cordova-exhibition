ebd100e2a72a9a175caa73dcfd6d479b9e7671e3
hazems <hazems@apache.org>
2014-06-02 03:26:55 +0300

    Adding Notification Demo

---
+++ www/js/api/NotificationManager.js
@@ -0,0 +1,34 @@
+//Singleton Object
+var NotificationManager = (function () {     
+  var instance;
+ 
+  function createObject() {
+      return {
+          showAlert: function (message, callback, title, buttonName) {
+              navigator.notification.alert(message, callback, title, buttonName);
+          },
+          showConfirm: function (message, callback, title, buttonLabels) {
+        	  navigator.notification.confirm(message, callback, title, buttonLabels);
+          },
+          showPrompt: function (message, callback, title, buttonLabels, defaultText) {
+        	  navigator.notification.prompt(message, callback, title, buttonLabels, defaultText);
+          },
+          beep: function (times) {
+        	  navigator.notification.beep(times);
+          },
+          vibrate: function (milliseconds) {
+        	  navigator.notification.vibrate(milliseconds);
+          }          
+      };
+  };
+ 
+  return {
+    getInstance: function () {
+      if (!instance) {
+          instance = createObject();
+      }
+ 
+      return instance;
+    }
+  }; 
+})();
